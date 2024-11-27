ee3d8288517a670b7a1237e7d4946cdff641d258
maju1717 <majuzin2007@gmail.com>
2024-11-27 09:22:24 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Maquiagem',
+    'Quais os produtos essenciais para uma maquiagem leve?',
+    'é necessário uma base BB cream, um corretivo cobertura leve,pó translúcido, blush, rímel um gloss ou batom.'
+)
+
+criaCartao(
+    'SkinCare',
+    'Quais são os passos básicos de uma rotina de cuidados com a pele?',
+    'Limpeza, tonificação, aplicaçaõ do sérum, hidratação, e protetor solar.'
+)
+
+criaCartao(
+    'Cabelo',
+    'Como fazeruma rotina de cuiddaos para cabelos oleosos?',
+    'Use shampoo específicos para controle da oleosidade, lave-o regularmente, evite condicionadores na raiz, use produtos leves e sem óleo na fórmula.'
+)
+
+criaCartao(
+    'Unha',
+    'Quais são os sinais de que suas unhas precisam de mais cuidados.?',
+    'Quando estão quebradiças, descamação, manchas ou cuticulas secas ou rachadas.'
+)
